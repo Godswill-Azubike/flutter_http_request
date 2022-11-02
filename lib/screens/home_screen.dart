528ace273b120ade45bb1625067a3e8bee7feb7e
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2022-11-02 10:47:20 +0100

    chore: fetch post data from api

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_http/models/user_model.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_http/screens/comment_screen.dart';
+import 'package:flutter_http/screens/post_screen.dart';
+import 'package:flutter_http/screens/user_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,77 +17,48 @@ class _HomeScreenState extends State<HomeScreen> {
     color: Colors.blue,
   );
 
-  Future getUser() async {
-    var response = await http.get(Uri.https("jsonplaceholder.typicode.com", 'users'));
-    // print(response.body);
-    var jsonData = jsonDecode(response.body);
-    // print(jsonData);
-
-    // print(jsonData[1]['website']);
-    List<User> users = [];
-
-    for (var u in jsonData) {
-      User user = User(u['name'], u['email'], u['username'], u['phone']);
-      users.add(user);
-    }
-
-    // print(users.length);
-
-    return users;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Http Users"),
+        title: const Text("Home Screen"),
       ),
-      body: Card(
-        child: FutureBuilder(
-          future: getUser(),
-          builder: (context, snapshot) {
-            if (snapshot.data == null) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const CircularProgressIndicator(),
-                    Text("Loading ...", style: myStyle),
-                  ],
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserScreen(),
                 ),
               );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {},
-                    title: Text(snapshot.data[index].name, style: myStyle),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(snapshot.data[index].email),
-                        Text(snapshot.data[index].phoneName),
-                      ],
-                    ),
-                    trailing: Text(snapshot.data[index].userName),
-                  );
-                },
-              );
-            }
-          },
-        ),
-      ),
-      // body: Center(
-      //   child: GestureDetector(
-      //     onTap: () async => await getUser(),
-      //     child: Text(
-      //       "No User found",
-      //       style: myStyle,
-      //     ),
-      //   ),
-      // ),
+            },
+            child: const Text('All Users'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CommentScreen(),
+              ),
+            ),
+            child: const Text('All Comments'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const PostScreen(),
+              ),
+            ),
+            child: const Text('Post'),
+          )
+        ],
+      )),
     );
   }
 }
